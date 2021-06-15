df97b5d2811115f45fae0030e996d79b906c10ca
Kamila <wiewiora145@gmail.com>
2021-06-15 21:56:13 +0200

    zarejestrowanie user-service w eureka

--- user/src/main/java/skamila/doctor24/user/UsersApplication.java
+++ user/src/main/java/skamila/doctor24/user/UsersApplication.java
@@ -2,8 +2,10 @@ package skamila.doctor24.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UsersApplication {
 
     public static void main(String[] args) {
