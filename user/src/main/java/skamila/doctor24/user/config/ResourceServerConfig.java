6aed77bd55f157545e572d8dddbe9924235b4d08
Kamila <wiewiora145@gmail.com>
2021-06-20 12:51:05 +0200

    wyczyszczenie user service po pozostałościach z nieudanych prób połączenia z auth service

--- user/src/main/java/skamila/doctor24/user/config/ResourceServerConfig.java
+++ user/src/main/java/skamila/doctor24/user/config/ResourceServerConfig.java
@@ -1,13 +1,9 @@
 package skamila.doctor24.user.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @EnableGlobalMethodSecurity(jsr250Enabled = true)
 @EnableResourceServer
@@ -15,14 +11,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-
-        http
-                .authorizeRequests()
-                        .antMatchers("/users/register").permitAll()
-                        .anyRequest().authenticated();
-//                .exceptionHandling(e -> e
-//                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-//                )
-//                .oauth2Login();
+        http.authorizeRequests()
+            .antMatchers("/users/register").permitAll()
+                .anyRequest().authenticated();
     }
 }
